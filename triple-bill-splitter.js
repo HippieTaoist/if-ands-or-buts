7726c1e3d8133569008e0549b81e9f51549dbfa7
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-01 12:27:48 -0600

    Enter Instructional Code

---
+++ triple-bill-splitter.js
@@ -0,0 +1,3 @@
+// ## Extra Credit: `triple-bill-splitter.js`
+
+// Write a new program that expands on `bill-splitter.js` this program should be able to split a bill between three people! Once again, prompt the user for how much each person paid. Then, log out messages indicating who needs to pay who.
